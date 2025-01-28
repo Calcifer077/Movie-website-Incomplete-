a4700fe248f806f0bdd5f07b70ef264bac09e8c1
Calcifer077 <maheshnashier14@gmail.com>
2025-01-28 17:52:29 +0530

    Installed new version of mongoose

--- Server/server.js
+++ Server/server.js
@@ -2,19 +2,16 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-// 'dotenv' is basically used to get info from config file.
-// In the below line we are telling it where 'config' file is located.
 dotenv.config({ path: './config.env' });
 
-mongoose
-  .connect(process.env.DATABASE_LOCAL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('Database connection successfull!'));
+const DB = process.env.DATABASE_ATLAS.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD_ATLAS,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successfull'));
 
 const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
-  console.log(`App running on ${port}`);
+  console.log(`App running on port ${port}`);
 });
