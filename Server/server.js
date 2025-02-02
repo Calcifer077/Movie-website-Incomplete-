50e0e1cf5082d064b2cb40f70f0dfa087790aadd
Calcifer077 <maheshnashier14@gmail.com>
2025-02-02 18:30:35 +0530

    Updated database so that it can connect to both locally and on cloud

--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,21 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE_ATLAS.replace(
-  '<db_password>',
-  process.env.DATABASE_PASSWORD_ATLAS,
-);
+// When connecting to ATLAS
+// const DB = process.env.DATABASE_ATLAS.replace(
+//   '<db_password>',
+//   process.env.DATABASE_PASSWORD_ATLAS,
+// );
 
-mongoose.connect(DB).then(() => console.log('DB connection successfull'));
+// mongoose.connect(DB).then(() => console.log('DB connection successfull'));
+
+// When connecting locally
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
